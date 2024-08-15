5159779f63c418c72723dc515482c150408b4f40
Mauro Gestoso <maurogestoso@gmail.com>
2024-08-15 23:43:38 +0200

    Implement guess colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  React.useEffect(() => {
-    document.addEventListener("keyup", (event) => {
-      console.log("🚀 ~ document.addEventListener ~ event.key:", event.key);
-      if (event.key === "Backspace") return dispatch({ type: "BACKSPACE" });
-      if (event.key === "Enter") return dispatch({ type: "GUESS_WORD" });
-      if (/^[a-zA-Z]$/.test(event.key))
-        return dispatch({ type: "TYPE_LETTER", payload: event.key });
-    });
-  }, []);
-
   const targetWord = "apple";
+  const [{ guesses, currentGuessIndex }, dispatch] = React.useReducer(
+    guessesReducer,
+    {
+      currentGuessIndex: 0,
+      guesses: [""],
+    }
+  );
+  useKeyEventListener(dispatch);
 
-  const [{ guesses }, dispatch] = React.useReducer(guessesReducer, {
-    currentGuessIndex: 0,
-    guesses: [""],
-  });
-
-  // state: guesses = string[]
   return (
     <>
       <div>
@@ -27,7 +19,13 @@ function App() {
         <h2>Guesses:</h2>
         <ol>
           {guesses.map((guess, i) => (
-            <li key={i}>{guess}</li>
+            <li key={i}>
+              <WordTiles
+                word={guess}
+                targetWord={targetWord}
+                isGuessed={i !== currentGuessIndex}
+              />
+            </li>
           ))}
         </ol>
       </div>
@@ -36,6 +34,30 @@ function App() {
 }
 
 export default App;
+
+function WordTiles({ word, isGuessed, targetWord }) {
+  function getGuessColor(targetWord, letter, index) {
+    if (targetWord[index] === letter) return "green";
+    if (targetWord.includes(letter)) return "yellow";
+    else return "gray";
+  }
+
+  return (
+    <div className="WordTiles">
+      {word.split("").map((letter, i) => (
+        <div
+          key={i}
+          className={[
+            "Tile",
+            isGuessed && `bg-${getGuessColor(targetWord, letter, i)}`,
+          ].join(" ")}
+        >
+          {letter}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function guessesReducer(state, action) {
   let newGuesses;
@@ -62,4 +84,16 @@ function guessesReducer(state, action) {
       };
   }
   return state;
+}
+
+function useKeyEventListener(dispatch) {
+  React.useEffect(() => {
+    document.addEventListener("keyup", (event) => {
+      console.log("🚀 ~ document.addEventListener ~ event.key:", event.key);
+      if (event.key === "Backspace") return dispatch({ type: "BACKSPACE" });
+      if (event.key === "Enter") return dispatch({ type: "GUESS_WORD" });
+      if (/^[a-zA-Z]$/.test(event.key))
+        return dispatch({ type: "TYPE_LETTER", payload: event.key });
+    });
+  }, []);
 }
