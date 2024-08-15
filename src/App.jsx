acc7def653ce059498d3a092a3fe559871bdfa9e
Mauro Gestoso <maurogestoso@gmail.com>
2024-08-15 22:17:29 +0200

    Vite boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Wordle</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
